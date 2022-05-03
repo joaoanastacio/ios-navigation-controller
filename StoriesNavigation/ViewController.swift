eb7763cc7e8218a76c579428037914e1e223a8f6
Joao Victor Anastacio <profissional.anastacio@gmail.com>
2022-05-02 21:26:33 -0700

    Initial Commit

---
+++ StoriesNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoriesNavigation
+//
+//  Created by Joao Victor Silva Anastacio on 2022-05-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
